fef5c989c0864e2935e2d99d6a83197de64d36aa
Scott MacLellan <scott.maclellan@vidyard.com>
2023-01-02 17:03:35 -0500

    Cleaning up the main game

--- pages/index.vue
+++ pages/index.vue
@@ -1,38 +1,6 @@
 <template>
   <div class="grid grid-cols-3 grid-rows-4 h-screen">
-    <div class="col-span-3 row-start-4 text-2xl">
-      <div v-if="connectionMode === 'Lobby'" class="my-4">
-        <strong>Connection:</strong>
-        <button class="btn btn-blue" @click="host(DefaultGame)">Host</button>
-        <button class="btn btn-blue" @click="join(DefaultGame)">Join</button>
-      </div>
-      <div v-if="connectionMode === 'Host'" class="my-4">
-        <button
-          v-if="mode === 'Dealing' || mode === 'Game'"
-          class="btn btn-blue"
-          @click="deal"
-        >
-          Start
-        </button>
-        <template v-if="debug">
-          <p><strong>Mode:</strong> {{ mode }}</p>
-          <button class="btn btn-blue" :disabled="!ready" @click="next">
-            Next
-          </button>
-        </template>
-      </div>
-      <div class="my-4">
-        <p>
-          <strong>Trump:</strong> {{ trump }}
-          <span :class="[red ? 'text-red-500' : 'text-black']">{{
-            emoji
-          }}</span>
-        </p>
-        <p><strong>Us:</strong> {{ redScore }}</p>
-        <p><strong>Them:</strong> {{ blueScore }}</p>
-      </div>
-      <BiddingControls v-if="mode === 'Bidding'" class="my-4" />
-    </div>
+    <GameControls class="col-span-3 row-start-4" />
     <PlayArea
       class="col-start-2 row-start-2"
       :bottom="players[0].played"
@@ -73,71 +41,39 @@
 
 <script lang="ts">
 import { storeToRefs } from 'pinia';
-import { computed, defineComponent } from 'vue';
-import {
-  ConnectionMode,
-  useConnectionsStore,
-  DefaultGame,
-} from '@/store/connections';
+import { defineComponent } from 'vue';
+import { useConnectionsStore, DefaultGame } from '@/store/connections';
 import { useGameStore } from '@/store/game';
 import { getDebugSettings } from '@/store/debug';
 import wait from '@/store/waiter';
-import { Emojis, getPerfectDeck, isRed, shuffle } from '~/CardTypes';
 
 export default defineComponent({
   name: 'IndexPage',
   setup() {
-    const { debug, solo } = getDebugSettings();
+    const { debug } = getDebugSettings();
 
     const connections = useConnectionsStore();
-    const { localId, mode: connectionMode } = storeToRefs(connections);
-    const { host, join } = connections;
+    const { localId } = storeToRefs(connections);
 
     const store = useGameStore();
     const { players, mode, ready, active, trump, redScore, blueScore } =
       storeToRefs(store);
-    const { bid, play, next, start } = store;
-
-    const red = computed(() => isRed(trump.value));
-    const emoji = computed(() => Emojis[trump.value]);
+    const { play } = store;
 
     wait(store);
-
-    let dealer: number = Math.floor(Math.random() * 4);
-    function deal() {
-      if (connectionMode.value !== ConnectionMode.Host && !debug) {
-        throw new Error('Not host');
-      }
-
-      dealer++;
-      const deck = shuffle(getPerfectDeck());
-      start(0, dealer % 4, deck);
-    }
-
-    if (solo) {
-      deal();
-    }
 
     return {
       debug,
       DefaultGame,
-      connectionMode,
       localId,
-      host,
-      join,
       players,
       mode,
       ready,
       active,
-      red,
-      emoji,
       trump,
       redScore,
       blueScore,
-      deal,
-      bid,
       play,
-      next,
     };
   },
 });
