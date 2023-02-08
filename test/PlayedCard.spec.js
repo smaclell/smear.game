86e9f1f87f7fd68bacb2008d7603e269a27cf43d
Scott MacLellan <s.j.maclell@gmail.com>
2023-02-08 17:38:30 -0500

    Making this test more realistic

--- test/PlayedCard.spec.js
+++ test/PlayedCard.spec.js
@@ -3,7 +3,14 @@ import PlayedCard from '@/components/PlayedCard.vue';
 
 describe('PlayedCard', () => {
   test('is a Vue instance', () => {
-    const wrapper = mount(PlayedCard);
+    const wrapper = mount(PlayedCard, {
+      props: {
+        card: { suit: 'Hearts', value: 11 },
+        hide: false,
+        allowed: true,
+        trump: true,
+      },
+    });
     expect(wrapper.vm).toBeTruthy();
   });
 });
