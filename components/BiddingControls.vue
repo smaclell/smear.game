b08234075f79c26a1c8c61fd0b6c001b4cf041fa
Scott MacLellan <s.j.maclell@gmail.com>
2023-02-08 19:35:05 -0500

    Better bidd

--- components/BiddingControls.vue
+++ components/BiddingControls.vue
@@ -1,9 +1,11 @@
 <template>
   <div>
     <div class="text-center py-4">
-      <strong>Your turn to Bid</strong>
+      <strong v-if="show">Your turn to Bid</strong>
+      <strong v-else-if="best > 0">{{ winner }} bid {{ best }}</strong>
+      <strong v-else>Still waiting for that first bid ...</strong>
     </div>
-    <div class="text-center py-4">
+    <div v-if="show" class="text-center py-4">
       <button
         class="btn btn-blue"
         :disabled="best >= 2 || blocked"
@@ -26,7 +28,7 @@
         4
       </button>
     </div>
-    <div class="text-center py-4">
+    <div v-if="show" class="text-center py-4">
       <button class="btn btn-blue" :disabled="blocked" @click="bid(active, -1)">
         Pass
       </button>
@@ -43,9 +45,18 @@ import { getDebugSettings } from '@/store/debug';
 const { debug } = getDebugSettings();
 
 const store = useGameStore();
-const { active, ready, maxBid } = storeToRefs(store);
+const { active, ready, players, maxBid } = storeToRefs(store);
 const { bid } = store;
 
-const blocked = computed(() => !debug && !ready.value && active.value !== 0);
+const show = computed(() => debug || active.value === 0);
+const blocked = computed(() => !debug && !ready.value && !show.value);
 const best = computed(() => maxBid.value[0]);
+const winner = computed(() => {
+  const max = maxBid.value[1];
+  if (best.value <= 0 || max < 0 || max >= players.value.length) {
+    return null;
+  }
+
+  return players.value[max].name;
+});
 </script>
